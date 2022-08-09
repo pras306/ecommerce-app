e97b66d51a50f1087958c128f7104519f811d3fb
pras306 <44067654+pras306@users.noreply.github.com>
2022-08-10 03:01:19 +0530

    MODIFY Commit
    - Updated Home container to display all products in catalogue
    - Updated the styling for the container as well

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Home.css';
+import { useGetProductsQuery } from '../../features/Products/ProductsSlice';
+import { ProductList } from '../../components';
 
 const Home = () => {
+    const { data: productList, isFetching } = useGetProductsQuery();
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(productList?.products);
+    }, [productList]);
+
+    const renderPage = () => {
+        if(isFetching) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div className='app__home'>
+                    <ProductList products={products} />
+                </div>
+            );
+        }
+    }
+
     return (
-        <div>Welcome to Home Page</div>
+        <>
+            {renderPage()}
+        </>
     );
 };
 
