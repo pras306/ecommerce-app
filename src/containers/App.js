ed72901fde7e4e5f2f022719b5a0b65b3dbafd85
pras306 <44067654+pras306@users.noreply.github.com>
2022-08-12 03:07:30 +0530

    MODIFY Commit
    - Modified App.js to implement Loader and Modal components in App's home

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import './App.css';
-import { Header } from '../components';
+import { Header, Modal, Loader } from '../components';
 import Main from './Main/Main';
 import { calculateTotals } from '../features/Cart/CartSlice';
 
 const App = () => {
   const dispatch = useDispatch();
   const { cartItems } = useSelector(store => store.cart);
+  const { isOpen } = useSelector(store => store.modal);
+  const { isLoading } = useSelector(store => store.loader);
 
   useEffect(() => {
     dispatch(calculateTotals());
@@ -18,6 +20,12 @@ const App = () => {
   return (
     <div className="app">
       <Router>
+        {
+          isLoading && <Loader />
+        }
+        {
+          isOpen && <Modal />
+        }
         <Header />
         <Main />
       </Router>
